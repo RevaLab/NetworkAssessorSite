769942c29fabd099aca788322b169c5a28f68c27
Elliott Grieco <elliott.grieco@nyu.edu>
2019-08-13 18:06:05 -0400

    Hides query list upon filtering

--- client/src/components/QueryUI.js
+++ client/src/components/QueryUI.js
@@ -4,33 +4,36 @@ import simpleAction from '../actions/simpleAction';
 
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import {
-  Columns,
   Container,
   Section,
+  Form,
+  Columns,
+  Button,
 } from 'react-bulma-components';
+import { Collapse } from 'react-collapse';
+import Switch from 'react-bulma-switch/full';
 
 import QueryList from './QueryList';
-import FilteredGenes from './FilteredGenes';
 import GeneFilterTool from './GeneFilterTool';
+
+
+const { Field, Control } = Form;
 
 class QueryUI extends React.Component {
   state = {
+    value: '',
     queryGenes: [],
-    filteredGenes: {
-
-    },
+    filteredGenes: {},
     filtering: false,
   }
 
-  updateQueryGenes = (queryGenes) => {
+  onChange = evt => {
     this.setState({
-      queryGenes,
-      filteredGenes: Object.fromEntries(queryGenes.map(gene => [
-        gene,
-        true,
-      ])),
-    });
-  }
+        queryGenes: evt.target.value.split('\n').filter(el => el),
+        value: evt.target.value,
+      }
+    );
+  };
 
   updateFilteredGenes = (filteredGenes) => {
     this.setState({
@@ -39,10 +42,10 @@ class QueryUI extends React.Component {
   }
 
   toggleFiltering = () => {
-    if (!this.state.queryGenes.length) return;
     this.setState({
+      filteredGenes: this.state.queryGenes.reduce((acc, gene) => ({...acc, [gene]: true }), {}),
       filtering: !this.state.filtering,
-    })
+    });
   }
 
   toggleFiltered = (gene, value) => {
@@ -54,28 +57,92 @@ class QueryUI extends React.Component {
     })
   }
 
+  handleExample = () => {
+    const exampleGenes = [
+      "FLT3",
+      "SMO",
+      "GLA",
+      "SGCB",
+      "OAT",
+      "CAPN3",
+      "ASS1",
+      "AGXT",
+      "AKT1",
+      "PTPN1",
+      "PIAS1",
+      "CDKN1B",
+      "THEM4",
+      "CCNE1",
+      "MAP2K4",
+      "ATG7",
+      "ATG12",
+      "BAD",
+      "BCL2L1",
+    ];
+
+    this.setState({
+      value: exampleGenes.join('\n'),
+      queryGenes: exampleGenes,
+      filtering: true,
+      filteredGenes: exampleGenes.reduce((acc, gene) => ({...acc, [gene]: true }), {}),
+    })
+  }
+
   render() {
-    const filteredGenesList =
-      Object.entries(this.state.filteredGenes).reduce((acc, [k, v]) => {
-        return v ? [...acc, k] : acc;
-      }, [])
+    const { filtering, queryGenes, value, filteredGenes } = this.state;
+    const switchText = filtering ? 'Update Query List' : 'Filter genes';
+
     return (
       <div>
         <Section>
-          <Container breakpoint="widescreen">
-            <QueryList
-              genes={this.state.queryGenes}
-              updateGenes={this.updateQueryGenes}
-              filtering={this.state.filtering}
-              toggleFiltering={this.toggleFiltering}
-            />
-          </Container>
+            <Container breakpoint="widescreen">
+              <Field>
+                <Collapse isOpened={!filtering}>
+                  <QueryList
+                    filtering={filtering}
+                    value={value}
+                    onChange={this.onChange}
+                    genes={queryGenes}
+                  />
+                </Collapse>
+              </Field>
+              <Field>
+                <Control>
+                  <Columns>
+                    <Columns.Column size="half">
+                      <Field>
+                        <Control>
+                          <Switch
+                            id="filter-genes-toggle"
+                            checked={filtering}
+                            onChange={this.toggleFiltering}
+                            disabled={!queryGenes.length}
+                          >
+                          {switchText}
+                          </Switch>
+                        </Control>
+                      </Field>
+                    </Columns.Column>
+                    <Columns.Column size="half">
+                      {
+                        queryGenes.length || filtering ? null :
+                        <Button color="info" fullwidth
+                          onClick={this.handleExample}
+                        >
+                          Try Example
+                        </Button>
+                      }
+                    </Columns.Column>
+                  </Columns>
+                </Control>
+              </Field>
+            </Container>
         </Section>
-        { this.state.filtering &&
+        { filtering &&
           <Section>
             <GeneFilterTool
-              queryGenes={this.state.queryGenes}
-              filteredGenes={this.state.filteredGenes}
+              queryGenes={queryGenes}
+              filteredGenes={filteredGenes}
               toggleFiltered={this.toggleFiltered}
             />
           </Section>
