ccd861cca8a4b3425183f5fee4b04b125c1bb981
Elliott Grieco <elliott.grieco@nyu.edu>
2019-08-14 13:51:45 -0400

    Fixes spacing

--- client/src/components/GeneFilterTool.js
+++ client/src/components/GeneFilterTool.js
@@ -40,41 +40,40 @@ class GeneFilterTool extends React.Component {
   }
 
   render() {
+    const filteredGoTerms = {
+      byId: this.state.goTerms.byId,
+      allIds: this.state.selectedTerms,
+    }
 
-  const filteredGoTerms = {
-    byId: this.state.goTerms.byId,
-    allIds: this.state.selectedTerms,
+    return (
+      <Container className="GeneFilterTool">
+        {
+          !!this.state.ontologies.allIds.length &&
+          !!this.state.selectedTerms.length &&
+          <GeneFilterContainer
+            ontologies={this.state.ontologies}
+            goTerms={filteredGoTerms}
+          />
+        }
+        {
+          this.state.ontologies.allIds.length ?
+          <GeneFilterContainer
+            ontologies={this.state.ontologies}
+            goTerms={this.state.goTerms}
+          />
+          :
+          <Loader style={{
+            width: 100,
+            height: 100,
+            border: '4px solid #209cee',
+            borderTopColor: 'transparent',
+            borderRightColor: 'transparent',
+            margin: '0 auto',
+          }}/>
+        }
+      </Container>
+    );
   }
-
-  return (
-    <Container className="GeneFilterTool">
-      {
-        !!this.state.ontologies.allIds.length &&
-        !!this.state.selectedTerms.length &&
-        <GeneFilterContainer
-          ontologies={this.state.ontologies}
-          goTerms={filteredGoTerms}
-        />
-      }
-      {
-        this.state.ontologies.allIds.length ?
-        <GeneFilterContainer
-          ontologies={this.state.ontologies}
-          goTerms={this.state.goTerms}
-        />
-        :
-        <Loader style={{
-          width: 100,
-          height: 100,
-          border: '4px solid #209cee',
-          borderTopColor: 'transparent',
-          borderRightColor: 'transparent',
-          margin: '0 auto',
-        }}/>
-      }
-    </Container>
-  );
-}
 }
 
 export default GeneFilterTool;
