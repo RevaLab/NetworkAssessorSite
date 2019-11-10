a442f24f8fbf80b5b3f2aa5ff88f419531612da8
Elliott Grieco <elliott.grieco@gmail.com>
2019-11-09 22:06:12 -0500

    Click outside of color picker to close

--- client/src/components/NetworkUI/ColorPicker/ColorPicker.js
+++ client/src/components/NetworkUI/ColorPicker/ColorPicker.js
@@ -1,12 +1,21 @@
-import React, { useEffect, useCallback } from 'react'
+import React, { useEffect, useCallback, useRef } from 'react'
 import { CompactPicker } from 'react-color';
 
-const ColorPicker = ({ color, pathwayId, setColorPicker, onChangeComplete }) => {
+const ColorPicker = ({ color, setColorPicker, onChangeComplete }) => {
+  const ref = useRef(null)
+
   const handleClose = useCallback((e) => {
     if (e.target === e.currentTarget) {
       e.stopPropagation();
       setColorPicker(null);
     }
+  }, [setColorPicker])
+
+  const handleClick = useCallback(e => {
+    if (ref.current.contains(e.target)) {
+      return
+    }
+    setColorPicker(null);
   }, [setColorPicker])
 
   const handleKey = useCallback((e) => {
@@ -18,15 +27,19 @@ const ColorPicker = ({ color, pathwayId, setColorPicker, onChangeComplete }) => 
   }, [setColorPicker])
 
   useEffect(() => {
+    document.addEventListener('mousedown', handleClick, false)
     document.addEventListener('keydown', handleKey, false);
 
     return () => {
+      document.removeEventListener('mousedown', handleClick, false)
       document.removeEventListener('keydown', handleKey, false)
     }
-  }, [handleKey])
+  }, [handleClick, handleKey])
+
+
 
   return (
-    <div className="ColorPicker">
+    <div className="ColorPicker" ref={ref}>
       <div className="compact-picker-overlay" onClick={handleClose} />
       <div className="compact-picker-container">
         <CompactPicker
