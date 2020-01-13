8222a6947f3cf58643922a717117d6f632332d2c
annapamma <anna.pamela@gmail.com>
2020-01-12 19:41:57 -0500

    Serve first degree of selected pathways

--- server/app.py
+++ server/app.py
@@ -52,8 +52,8 @@ def go_terms():
 
 @app.route('/api/table', methods=['POST'])
 def table():
-    if os.environ.get('USE_MOCKS'):
-        return mock_table(request)
+    # if os.environ.get('USE_MOCKS'):
+    #     return mock_table(request)
 
     return jsonify(tbl.table(request, min_max))
 
@@ -68,48 +68,15 @@ def network():
         "selectedPathways": [333, 353, 2]
     }
     """
-    genes = request.json['genes']
+    query_list = request.json['genes']
     ppi_db = graphs[request.json['db']]
     selected_pw_db = request.json['selectedPathwayDatabase']
-    selected_pathways = set(int(n) for n in set(request.json['selectedPathways']))
+    selected_pathways = set(int(n) for n in request.json['selectedPathways'])
+    genes = tbl.network_genes(query_list, selected_pathways, ppi_db, selected_pw_db)
     res = {
-        "nodes": nodes(genes, ppi_db, selected_pw_db, selected_pathways, genes),
+        "nodes": nodes(genes, ppi_db, selected_pw_db, selected_pathways, query_list),
         "links": links(genes, ppi_db)
     }
-    # print(res)
-    # res = {
-    #     "nodes": [
-    #         {
-    #         "id": "AKT1",
-    #         "pieChart": [
-    #             { "color": 0, "percent": 50 },
-    #             { "color": 324, "percent": 50 }
-    #         ]
-    #         },
-    #         {
-    #         "id": "BAD",
-    #         "pieChart": [
-    #             { "color": 3369, "percent": 33.33 },
-    #             { "color": 3116, "percent": 33.34 },
-    #             { "color": 2942, "percent": 33.33 }
-    #         ]
-    #         },
-    #         {
-    #         "id": "BCL2L1",
-    #         "pieChart": [
-    #             { "color": 1210, "percent": 25 },
-    #             { "color": 1911, "percent": 25 },
-    #             { "color": 1097, "percent": 25 },
-    #             { "color": 2725, "percent": 25 }
-    #         ]
-    #         }
-    #     ],
-    #     "links": [
-    #         { "source": "AKT1", "target": "BAD" },
-    #         { "source": "BAD", "target": "BCL2L1" },
-    #         { "source": "BCL2L1", "target": "AKT1" },
-    #     ]
-    # }
     return jsonify(res)
 
 
@@ -140,107 +107,107 @@ def links(gene_list, g):
     } for gene in gene_list for neighbor in set(g.neighbors(gene)).intersection(gene_list)]
 
 
-def mock_table(request):
-    selected_pathway_db = request.json['selectedPathwayDatabase']
-
-    pathway_sources_delays = {
-        'KEGG': 15,
-        'My Cancer Genome': 3,
-        'Reactome': 25
-    }
-
-    sleep(pathway_sources_delays[selected_pathway_db])
-    return jsonify({
-        "tableData": [
-            {
-                "id": "3379",
-                "name": "WNT ext path",
-                "color": "#fe5d18",
-                "membersLength": 12,
-                "overlapLength": 5,
-                "edgesLength": 5,
-                "pVal": 0.0003250272196074229
-            },
-            {
-                "id": "3380",
-                "name": "CALC PKC ext path",
-                "color": "#923b3e",
-                "membersLength": 5,
-                "overlapLength": 8,
-                "edgesLength": 8,
-                "pVal": 0.0013787906109511874
-            },
-            {
-                "id": "4903",
-                "name": "Jack Stat ext path",
-                "color": "#82fd0f",
-                "membersLength": 14,
-                "overlapLength": 3,
-                "edgesLength": 3,
-                "pVal": 0.0006057200026052585
-            },
-            {
-                "id": "5290",
-                "name": "Mitogen Activated Protein-MAP Kinase Signaling path",
-                "color": "#4c7fb8",
-                "membersLength": 17,
-                "overlapLength": 5,
-                "edgesLength": 5,
-                "pVal": 0.0008449145721473537
-            },
-            {
-                "id": "6131",
-                "name": "Receptor Tyrosine KinaseORGrowth Factor Signaling path",
-                "color": "#521f74",
-                "membersLength": 14,
-                "overlapLength": 7,
-                "edgesLength": 7,
-                "pVal": 0.000026876482060089303
-            },
-            {
-                "id": "6145",
-                "name": "Protein Degradation Ubiquitination path",
-                "color": "#ec8600",
-                "membersLength": 7,
-                "overlapLength": 3,
-                "edgesLength": 3,
-                "pVal": 0.0009249305959767607
-            },
-            {
-                "id": "6194",
-                "name": "Kinase Fusions path",
-                "color": "#2b2e2c",
-                "membersLength": 9,
-                "overlapLength": 1,
-                "edgesLength": 1,
-                "pVal": 0.000954274430117962
-            },
-            {
-                "id": "6380",
-                "name": "AKT ext path",
-                "color": "#9561e2",
-                "membersLength": 26,
-                "overlapLength": 9,
-                "edgesLength": 9,
-                "pVal": 0.00185390326478682
-            },
-            {
-                "id": "6492",
-                "name": "G-Protein Signaling path",
-                "color": "#273b25",
-                "membersLength": 1,
-                "overlapLength": 6,
-                "edgesLength": 6,
-                "pVal": 0.000795553195050128
-            },
-            {
-                "id": "7388",
-                "name": "Hormone Signaling path",
-                "color": "#9dfc27",
-                "membersLength": 5,
-                "overlapLength": 3,
-                "edgesLength": 3,
-                "pVal": 0.0004654716728720567
-            }
-        ]
-    })
+# def mock_table(request):
+#     selected_pathway_db = request.json['selectedPathwayDatabase']
+#
+#     pathway_sources_delays = {
+#         'KEGG': 15,
+#         'My Cancer Genome': 3,
+#         'Reactome': 25
+#     }
+#
+#     sleep(pathway_sources_delays[selected_pathway_db])
+#     return jsonify({
+#         "tableData": [
+#             {
+#                 "id": "3379",
+#                 "name": "WNT ext path",
+#                 "color": "#fe5d18",
+#                 "membersLength": 12,
+#                 "overlapLength": 5,
+#                 "edgesLength": 5,
+#                 "pVal": 0.0003250272196074229
+#             },
+#             {
+#                 "id": "3380",
+#                 "name": "CALC PKC ext path",
+#                 "color": "#923b3e",
+#                 "membersLength": 5,
+#                 "overlapLength": 8,
+#                 "edgesLength": 8,
+#                 "pVal": 0.0013787906109511874
+#             },
+#             {
+#                 "id": "4903",
+#                 "name": "Jack Stat ext path",
+#                 "color": "#82fd0f",
+#                 "membersLength": 14,
+#                 "overlapLength": 3,
+#                 "edgesLength": 3,
+#                 "pVal": 0.0006057200026052585
+#             },
+#             {
+#                 "id": "5290",
+#                 "name": "Mitogen Activated Protein-MAP Kinase Signaling path",
+#                 "color": "#4c7fb8",
+#                 "membersLength": 17,
+#                 "overlapLength": 5,
+#                 "edgesLength": 5,
+#                 "pVal": 0.0008449145721473537
+#             },
+#             {
+#                 "id": "6131",
+#                 "name": "Receptor Tyrosine KinaseORGrowth Factor Signaling path",
+#                 "color": "#521f74",
+#                 "membersLength": 14,
+#                 "overlapLength": 7,
+#                 "edgesLength": 7,
+#                 "pVal": 0.000026876482060089303
+#             },
+#             {
+#                 "id": "6145",
+#                 "name": "Protein Degradation Ubiquitination path",
+#                 "color": "#ec8600",
+#                 "membersLength": 7,
+#                 "overlapLength": 3,
+#                 "edgesLength": 3,
+#                 "pVal": 0.0009249305959767607
+#             },
+#             {
+#                 "id": "6194",
+#                 "name": "Kinase Fusions path",
+#                 "color": "#2b2e2c",
+#                 "membersLength": 9,
+#                 "overlapLength": 1,
+#                 "edgesLength": 1,
+#                 "pVal": 0.000954274430117962
+#             },
+#             {
+#                 "id": "6380",
+#                 "name": "AKT ext path",
+#                 "color": "#9561e2",
+#                 "membersLength": 26,
+#                 "overlapLength": 9,
+#                 "edgesLength": 9,
+#                 "pVal": 0.00185390326478682
+#             },
+#             {
+#                 "id": "6492",
+#                 "name": "G-Protein Signaling path",
+#                 "color": "#273b25",
+#                 "membersLength": 1,
+#                 "overlapLength": 6,
+#                 "edgesLength": 6,
+#                 "pVal": 0.000795553195050128
+#             },
+#             {
+#                 "id": "7388",
+#                 "name": "Hormone Signaling path",
+#                 "color": "#9dfc27",
+#                 "membersLength": 5,
+#                 "overlapLength": 3,
+#                 "edgesLength": 3,
+#                 "pVal": 0.0004654716728720567
+#             }
+#         ]
+#     })
